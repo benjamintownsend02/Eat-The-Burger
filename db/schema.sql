32b9e56116bd2b39ee179d1211a4c127b7069016
Benjamin Townsend <benjamin.townsend.04@gmail.com>
2020-04-29 23:10:25 -0700

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,13 +4,11 @@ CREATE DATABASE burger_db;
 
 USE burger_db;
 
-CREATE TABLE plans (
-  id int NOT NULL AUTO_INCREMENT,
-  plan varchar(255) NOT NULL,
+CREATE TABLE burgers (
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
 
--- Insert a set of records.
-INSERT INTO plans (plan) VALUES ('Plan to fight a ninja.');
-
-SELECT * FROM plans;
+SELECT * FROM burgers;
