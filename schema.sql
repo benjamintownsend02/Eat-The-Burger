a7d94b9ab6d14d22cfc664c7df801b04e0da5d08
Benjamin Townsend <benjamin.townsend.04@gmail.com>
2020-04-29 21:36:59 -0700

    updated for heroku database

--- schema.sql
+++ schema.sql
@@ -10,10 +10,5 @@ CREATE TABLE burgers (
   devoured BOOLEAN NOT NULL,
   PRIMARY KEY (id)
 );
--- TEST CODE
--- UPDATE burgers SET devoured=0 WHERE id=1;
--- UPDATE burgers SET devoured=0 WHERE id=2;
--- UPDATE burgers SET devoured=0 WHERE id=3;
--- UPDATE burgers SET devoured=0 WHERE id=4;
 
 SELECT * FROM burgers;
